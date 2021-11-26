e7124b9bd8304ed3c45ee97906ee0a83fe0eb831
wb.wangjun21 <wb.wangjun21@mesg.corp.netease.com>
2021-11-26 15:13:20 +0800

    泛型

---
+++ 泛型/index.ts
@@ -0,0 +1,14 @@
+/**
+ * 泛型（Generics）是指在定义函数、接口或类的时候，不预先指定具体的类型，而在使用的时候再指定类型的一种特性。
+ */
+
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);
+    return arg;
+}
+
+loggingIdentity(8);
